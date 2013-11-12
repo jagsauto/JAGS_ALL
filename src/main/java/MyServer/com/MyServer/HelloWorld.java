112cc01f5713ef2bfac1dfa8d5efb97f7e969870
jags <jagadeesh_is@yahoo.com>
2013-11-12 23:16:58 +0000

    sample webapp

---
+++ src/main/java/MyServer/com/MyServer/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.MyServer;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+
+ 
+public class HelloWorld extends HttpServlet
+{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello Servlet</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+}
