24ecf77c28a05af8eb66319b83faf72e29373339
jags <jagadeesh_is@yahoo.com>
2013-11-12 23:47:31 +0000

    good changes

---
+++ src/main/java/MyServer/com/MyServer/HelloServlet.java
@@ -0,0 +1,38 @@
+package com.MyServer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        String q = req.getParameter("q");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("The paramter q was \"" + q + "\".");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+        String field = req.getParameter("field");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("You entered \"" + field + "\" into the text box.");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
